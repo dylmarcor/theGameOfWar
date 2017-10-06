d7fbef06040f1a5dcbbe40fb04c674f99ad94bba
Dylmarcor <dylancorbett@me.com>
2017-10-06 14:36:08 -0700

    Added JS game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+var startDeck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
@@ -9,22 +9,57 @@ Array.prototype.randomElement = function () {
 
 var playerOne = [];
 var playerTwo = [];
-var newDeck = [];
 
 function createDeck() {
-    for (var i = 0; i < myArray.length; i++) {
-        newDeck.push(myArray.randomElement());
+    var holder = [];
+    for (var i = 0; i < startDeck.length; i++) {
+        holder.push(startDeck.randomElement());
     }
-    playerOne = newDeck.splice(0, 26);
-    playerTwo = newDeck.splice(0, 26);
+    playerOne = holder.splice(0, 26);
+    playerTwo = holder;
 }
 
-createDeck();
-
-function drawCards() {
-    var playerOneMove, playerTwoMove;
-    playerOneMove = playerOne.randomElement();
-    console.log(playerOneMove);
+function battle(holder) {
+    checkWinner();
+    var p1 = playerOne.shift();
+    var p2 = playerTwo.shift();
+    if (p1 === p2) {
+        var holder = goToWar(p1, p2);
+        console.log(holder)
+    } else if (p1 > p2) {
+        playerOne.push(p1)
+        playerOne.push(p2)
+        if (holder) playerOne = playerOne.concat(holder)
+    } else {
+        playerTwo.push(p1)
+        playerTwo.push(p2)
+        if (holder) playerTwo = playerTwo.concat(holder)
+    }
 }
 
-drawCards();
+
+function goToWar(p1, p2) {
+    var holder = [p1, p2];
+    new Array(3).fill(null).forEach(() => holder.push(playerOne.shift()))
+    new Array(3).fill(null).forEach(() => holder.push(playerTwo.shift()))
+    battle(holder);
+    return holder;
+}
+
+function checkWinner() {
+    if (playerOne === []) {
+        return 2;
+        console.log("Player 1 wins")
+    } else if (playerTwo === []) {
+        return 1;
+        console.log("Player 2 wins")
+    } else {
+        return 0;
+    }
+}
+
+function init() {
+    createDeck();
+}
+
+init();
