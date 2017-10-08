4c5be37a1c312bcbfe5a0d69c02f52d8e74d4b28
Dylmarcor <dylancorbett@me.com>
2017-10-08 12:00:53 -0700

    Added JS and SVG cards

--- js/main.js
+++ js/main.js
@@ -1,60 +1,88 @@
-var startDeck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// var startDeck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+//     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+//     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+//     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+var startDeck = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
 
 Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)]
 }
 
-var playerOne = [];
-var playerTwo = [];
+var playerOne = [],
+    playerTwo = [],
+    holder = [];
 
 function createDeck() {
-    var holder = [];
+    var hold = [];
     for (var i = 0; i < startDeck.length; i++) {
-        holder.push(startDeck.randomElement());
+        hold.push(startDeck.randomElement());
     }
-    playerOne = holder.splice(0, 26);
-    playerTwo = holder;
+    playerOne = hold.splice(0, 8);
+    playerTwo = hold;
 }
+
+
 
 function battle(holder) {
     checkWinner();
+    var war;
     var p1 = playerOne.shift();
     var p2 = playerTwo.shift();
     if (p1 === p2) {
-        var holder = goToWar(p1, p2);
-        console.log(holder)
+        if (playerOne.length > 2 && playerTwo.length > 2) {
+            holder = [p1, p2];
+//             if (holder) holder.push(goToWar(p1, p2))
+            holder = holder.concat(goToWar(p1, p2));
+//             holder = holder.reduce(function(a,b) {return a.concat(b)}, []);
+            battle();
+        } else {
+            if (playerOne.length < 2) {
+                console.log("Player 1 wins!");
+            } else {
+                console.log("Player 2 wins!");
+            }
+        }
+            console.log(holder)
+            war = holder.concat(holder);
     } else if (p1 > p2) {
         playerOne.push(p1)
         playerOne.push(p2)
-        if (holder) playerOne = playerOne.concat(holder)
+        if (holder) {
+            playerOne.push(holder);
+//             playerOne = playerOne.reduce(function(a,b) {return a.concat(b), []});
+            console.log(holder);
+        }
     } else {
         playerTwo.push(p1)
         playerTwo.push(p2)
-        if (holder) playerTwo = playerTwo.concat(holder)
+        if (holder) {
+            playerTwo = playerTwo.concat(holder);
+//             playerTwo = playerTwo.reduce(function(a,b) {return a.concat(b), []});
+            console.log(holder);
+        }
     }
 }
 
 
 function goToWar(p1, p2) {
-    var holder = [p1, p2];
-    new Array(3).fill(null).forEach(() => holder.push(playerOne.shift()))
-    new Array(3).fill(null).forEach(() => holder.push(playerTwo.shift()))
-    battle(holder);
-    return holder;
+    var warChest = [];
+    new Array(1).fill(null).forEach( () => warChest.push(playerOne.shift()))
+    new Array(1).fill(null).forEach( () => warChest.push(playerTwo.shift()))
+    console.log(warChest);
+    return warChest;
 }
 
+
 function checkWinner() {
-    if (playerOne === []) {
-        return 2;
-        console.log("Player 1 wins")
-    } else if (playerTwo === []) {
-        return 1;
-        console.log("Player 2 wins")
-    } else {
+    if (playerOne.length === 0) {
+        console.log("Player 2 wins!")
         return 0;
+    } else if (playerTwo.length === 0) {
+        console.log("Player 1 wins!")
+        return 1;
+    } else {
+        return 2;
     }
 }
 
