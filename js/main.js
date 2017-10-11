695f38596eeeb977a20366592b4831e89fd82667
Dylmarcor <dylancorbett@me.com>
2017-10-11 15:41:18 -0700

    Formatted code

--- js/main.js
+++ js/main.js
@@ -14,11 +14,11 @@ Array.prototype.randomElement = function () {
 $('#reset').on('click', resetBoard)
 $('#draw').on('click', battle)
 
-$('.enter').click(function() {
+$('.enter').click(function () {
     $(this).parent().fadeOut(2000);
 })
 
-$('.enter').animate({opacity: 100}, 30000)
+$('.enter').animate({ opacity: 100 }, 30000)
 
 var playerOne = [],
     playerTwo = [],
@@ -42,7 +42,7 @@ function battle() {
     var p2 = playerTwo.shift();
     if (p1 === p2) {
         if (playerOne.length > 2 && playerTwo.length > 2) {
-            $('.battlefield').text('WAR').animate({color: 'white'}).fadeIn(80).fadeOut(80).fadeIn(80).fadeOut(80).fadeIn(80).fadeOut(80).fadeIn(80).animate({color: 'white'})
+            $('.battlefield').text('WAR').animate({ color: 'white' }).fadeIn(80).fadeOut(80).fadeIn(80).fadeOut(80).fadeIn(80).fadeOut(80).fadeIn(80).animate({ color: 'white' })
             setTimeout(2000);
             goToWar(p1, p2);
         } else {
@@ -63,14 +63,14 @@ function battle() {
         playerOne.push(p1)
         playerOne.push(p2)
         warChest = [];
-        $('.right-player').delay(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).animate({opacity: 0})
+        $('.right-player').delay(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).animate({ opacity: 0 })
         checkWinner();
-    } else { 
+    } else {
         if (warChest.length > 0) { playerTwo = playerTwo.concat(warChest) }
         playerTwo.push(p1)
         playerTwo.push(p2)
         warChest = [];
-        $('.left-player').delay(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).animate({opacity: 0})
+        $('.left-player').delay(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).animate({ opacity: 0 })
         checkWinner();
     }
 }
@@ -105,33 +105,33 @@ function checkWinner() {
 
 function render() {
     if (playerOne[0] === 14) {
-        $('.left-player').text('A').animate({opacity: 100});
+        $('.left-player').text('A').animate({ opacity: 100 });
     } else if (playerOne[0] === 13) {
-        $('.left-player').text('K').animate({opacity: 100});
+        $('.left-player').text('K').animate({ opacity: 100 });
     } else if (playerOne[0] === 12) {
-        $('.left-player').text('Q').animate({opacity: 100});
+        $('.left-player').text('Q').animate({ opacity: 100 });
     } else if (playerOne[0] === 11) {
-        $('.left-player').text('J').animate({opacity: 100});
+        $('.left-player').text('J').animate({ opacity: 100 });
     } else {
-        $('.left-player').text(playerOne[0]).animate({opacity: 100})
+        $('.left-player').text(playerOne[0]).animate({ opacity: 100 })
     }
     if (playerTwo[0] === 14) {
-        $('.right-player').text('A').animate({opacity: 100});
+        $('.right-player').text('A').animate({ opacity: 100 });
     } else if (playerTwo[0] === 13) {
-        $('.right-player').text('K').animate({opacity: 100});
+        $('.right-player').text('K').animate({ opacity: 100 });
     } else if (playerTwo[0] === 12) {
-        $('.right-player').text('Q').animate({opacity: 100});
+        $('.right-player').text('Q').animate({ opacity: 100 });
     } else if (playerTwo[0] === 11) {
-        $('.right-player').text('J').animate({opacity: 100});
+        $('.right-player').text('J').animate({ opacity: 100 });
     } else {
-        $('.right-player').text(playerTwo[0]).animate({opacity: 100})
+        $('.right-player').text(playerTwo[0]).animate({ opacity: 100 })
     }
     $('.right-deck').html('Deck:<br>' + playerTwo.length)
     $('.left-deck').html('Deck:<br>' + playerOne.length)
     $('#p1win').html(parseInt(p1WinCounter))
     $('#p2win').html(parseInt(p2WinCounter))
-    
-    
+
+
 
 }
 
